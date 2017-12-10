728e50929c378837261c857ce5bcd15437cf8f70
Craig Cravath <craigcravath@gmail.com>
2017-12-09 23:57:09 -0500

    add single class to run sorts from, clean up bubble to only contain sort implementation

--- bubble.py
+++ bubble.py
@@ -1,6 +1,3 @@
-import random
-
-
 def bubble(list):
     for list_size in range(len(list) - 1, 0, -1):
         for pos in range(list_size):
@@ -12,17 +9,3 @@ def swap(list, index1, index2):
     temp = list[index1]
     list[index1] = list[index2]
     list[index2] = temp
-
-
-def main():
-    sort_list = []
-    for x in range (0, 20):
-        sort_list.append(random.randint(0, 100))
-    print(sort_list)
-    print("-------")
-    bubble(sort_list)
-    print(sort_list)
-
-
-if __name__ == '__main__':
-    main()
---
+++ sort_runner.py
@@ -0,0 +1,16 @@
+import random
+import bubble
+
+
+def main():
+    sort_list = []
+    for x in range (0, 20):
+        sort_list.append(random.randint(0, 100))
+    print(sort_list)
+    print("-------")
+    bubble.bubble(sort_list)
+    print(sort_list)
+
+
+if __name__ == '__main__':
+    main()
